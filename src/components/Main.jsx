ad18ad75bac8241746b45ae1dfe2364d353b325b
ebcoleman <140854597+ebcoleman@users.noreply.github.com>
2024-04-18 11:29:18 -0400

    main is looking AWESOME

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,10 +7,10 @@ const Main = () => {
     return (
         <div id="main">
             <img className="w-full h-screen object-cover object-left scale-x-[-1]" src="/images/waterfall.jpg" />
-            <div className="w-full h-screen absolute top-0 left-0 bg-white/35">
-                <div>
-                    <h1>I'm Emily Coleman</h1>
-                    <h2>
+            <div className="w-full h-screen absolute top-0 left-0 bg-white/50">
+                <div className="max-w-[700px] m-auto h-full flex flex-col justify-center lg:items-start items-center">
+                    <h1 className="sm:text-5xl text-4xl font-bold text-gray-800">I'm Emily Coleman</h1>
+                    <h2 className="flex sm:text-3xl text-2xl pt-4 text-grey-800">
                         I'm a
                         <TypeAnimation
                         sequence={[
@@ -29,7 +29,7 @@ const Main = () => {
                         />
                         
                     </h2>
-                    <div>
+                    <div className="flex justify-between pt-6 max-w-[200px] w-full">
                         <a href="https://www.linkedin.com/in/emily-coleman-245893228/" target="_blank" rel="noopener noreferrer">   
                         <FaLinkedin className="cursor-pointer" size={20} />
                         </a>
