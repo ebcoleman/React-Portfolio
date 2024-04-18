a900e25d2435484d9e33b66091b8d79fd50df5b0
ebcoleman <140854597+ebcoleman@users.noreply.github.com>
2024-04-18 15:08:21 -0400

    contact form is functional.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Sidenav from './components/Sidenav'
 import Main from './components/Main'
 import About from './components/About'
 import Projects from './components/Projects'
+import Contact from './components/Contact'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
         <Main />
         <About />
         <Projects />
+        <Contact />
       </div>
   )
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const Contact = () => {
+    return (
+        <div id='contact' className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
+            <h1 className="py-4 text-4xl font-bold text-center text-[#4C783F]">Contact</h1>
+            <form 
+                action="https://getform.io/f/pbnvnvkb" 
+                method="POST" 
+                encType='multipart/form-data'
+            >
+                <div className="grid md:grid-cols-2 gap-4 w-full py-2">
+                    <div className="flex flex-col">
+                        <label className="uppercase text-sm">Name</label>
+                        <input className="border-2 rounded-lg p-3 flex border-gray-300" type="text" name="name" />
+                    </div>
+                    <div className="flex flex-col">
+                        <label className="uppercase text-sm">Phone</label>
+                        <input className="border-2 rounded-lg p-3 flex border-gray-300" type="text" name="phone" />
+                    </div>
+                </div>
+                <div className="flex flex-col py-2">
+                    <label className="uppercase text-sm">Email</label>
+                    <input className="border-2 rounded-lg p-3 flex border-gray-300" type="email" name="email" />
+                </div>
+                <div className="flex flex-col py-2">
+                    <label className="uppercase text-sm">Subject</label>
+                    <input className="border-2 rounded-lg p-3 flex border-gray-300" type="text" name="subject" />
+                </div>
+                <div className="flex flex-col py-2">
+                    <label className="uppercase text-sm">Message</label>
+                    <textarea className="border-2 rounded-lg p-3 border-gray-300" rows='10' name="message" ></textarea>
+                </div>
+                <button className="bg-[#4C783F] text-gray-100 mt-4 w-full p-4 rounded-lg">
+                    Send Message
+                </button>
+            </form>
+
+        </div>
+    )
+}
+
+export default Contact
