aa08eb9da306f778fca3803c779a0ccca800db87
ebcoleman <140854597+ebcoleman@users.noreply.github.com>
2024-04-19 12:14:27 -0400

    added the GitHub repo link below the images that will route to either the live application or a walkthrough video

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -15,24 +15,50 @@ const Projects = () => {
                 Please see below for a few samples of my work. 
             </p>
             <div className="grid sm:grid-cols-2 gap-12">
-                <a href="https://ebcoleman.github.io/Portfolio-Emily-Coleman/" target="_blank" rel="noopener noreferrer">  
+                <div>
+                    <a href="https://ebcoleman.github.io/Portfolio-Emily-Coleman/" target="_blank" rel="noopener noreferrer">  
                     <ProjectItem img={portfolioImg} title='First Portfolio App' />
-                </a>
-                <a href="https://github.com/ebcoleman/faceSpace" target="_blank" rel="noopener noreferrer">
-                    <ProjectItem img={faceSpaceImg} title='FaceSpace App' />
-                </a>
-                <a href="https://ebcoleman.github.io/password-generator/" target="_blank" rel="noopener noreferrer">
-                    <ProjectItem img={passwordImg} title='Password Generator App' />
-                </a>
-                <a href="https://ebcoleman.github.io/traveler-weather/" target="_blank" rel="noopener noreferrer">
-                    <ProjectItem img={weatherImg} title='Weather App' />
-                </a>
-                <a href="https://ebcoleman.github.io/Challenge-01/" target="_blank" rel="noopener noreferrer">
-                    <ProjectItem img={challengeImg} title='HTML/CSS App' />
-                </a>
-                <a href="https://github.com/ebcoleman/basic-logo-maker" target="_blank" rel="noopener noreferrer">
-                    <ProjectItem img={logoImg} title='SVG Logo Maker' />
-                </a>
+                    </a>
+                    
+                    <p className="text-center m-auto p-5 text-[#222E0F] font-semibold"><a href="https://github.com/ebcoleman/Portfolio-Emily-Coleman" target="_blank" rel="noopener noreferrer">GitHub Repo</a>
+                    </p>
+                </div>
+
+                <div>
+                    <a href="https://drive.google.com/file/d/1in-la5wquxNip-fzeHECTpnf7HBJKafj/view" target="_blank" rel="noopener noreferrer">
+                    <ProjectItem img={faceSpaceImg} title='FaceSpace App' /></a>
+
+                    <p className="text-center m-auto p-5 text-[#222E0F] font-semibold"> <a href="https://github.com/ebcoleman/faceSpace" target="_blank" rel="noopener noreferrer">GitHub Repo</a></p>
+                </div>
+
+                <div>
+                    <a href="https://ebcoleman.github.io/password-generator/" target="_blank" rel="noopener noreferrer">
+                    <ProjectItem img={passwordImg} title='Password Generator App' /></a>
+
+                    <p className="text-center m-auto p-5 text-[#222E0F] font-semibold"> <a href="https://github.com/ebcoleman/password-generator" target="_blank" rel="noopener noreferrer">GitHub Repo</a></p>
+                </div>
+
+                <div>
+                    <a href="https://ebcoleman.github.io/traveler-weather/" target="_blank" rel="noopener noreferrer">
+                    <ProjectItem img={weatherImg} title='Weather App' /></a>
+
+                    <p className="text-center m-auto p-5 text-[#222E0F] font-semibold"> <a href="https://github.com/ebcoleman/traveler-weather" target="_blank" rel="noopener noreferrer">GitHub Repo</a></p>
+                </div>
+
+                <div>
+                    <a href="https://ebcoleman.github.io/Challenge-01/" target="_blank" rel="noopener noreferrer">
+                    <ProjectItem img={challengeImg} title='HTML/CSS App' /></a>
+
+                    <p className="text-center m-auto p-5 text-[#222E0F] font-semibold"> <a href="https://github.com/ebcoleman/Challenge-01" target="_blank" rel="noopener noreferrer">GitHub Repo</a></p>
+                </div>
+
+                <div>
+                    <a href="https://drive.google.com/file/d/1Drjya71xg4XoS-iaF2IaqteFrKZC_PIn/view" target="_blank" rel="noopener noreferrer">
+                    <ProjectItem img={logoImg} title='SVG Logo Maker' /></a>
+
+                    <p className="text-center m-auto p-5 text-[#222E0F] font-semibold"> <a href="https://github.com/ebcoleman/basic-logo-maker" target="_blank" rel="noopener noreferrer">GitHub Repo</a></p>
+                </div>
+
             </div>
         </div>
     )
